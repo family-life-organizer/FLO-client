686d8f484da30c9d6e72fa7bd1a38848250f9696
renegade27 <foemaster@live.com>
2019-07-02 09:36:23 -0500

    Adding IconCard component.

--- src/components/application/card/IconCard.jsx
+++ src/components/application/card/IconCard.jsx
@@ -3,13 +3,27 @@ import styled from 'styled-components';
 
 const CardContainer = styled.div`
     background-color: lightgrey;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    height: 108px;
+    width: 100px;
+    .textbar {
+        width: 100%;
+        height: 24px;
+        background-color: "#2439A8";
+    }
 `;
 
 const IconCard = props => {
     const {text, Icon} = props;
     return (
         <CardContainer>
-            <Icon style={{height: '96px', width: '96px', color: "grey"}}/>
+            <Icon style={{height: '64px', width: '64px', color: "grey"}}/>
+            <div className="textbar">
+                <p>{text}</p>
+            </div>
         </CardContainer>
     );
 }
