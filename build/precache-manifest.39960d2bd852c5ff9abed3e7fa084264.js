81080ad5d612c9fff6185d562128e8a9dd80b902
renegade27 <foemaster@live.com>
2019-07-03 15:36:27 -0500

    Commit

---
+++ build/precache-manifest.39960d2bd852c5ff9abed3e7fa084264.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8992ae2d03bf4190fc3ba466f762d3b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f3c6f587916b093a5fd",
+    "url": "/static/css/2.ba1e7a52.chunk.css"
+  },
+  {
+    "revision": "9f3c6f587916b093a5fd",
+    "url": "/static/js/2.ea5c2ec3.chunk.js"
+  },
+  {
+    "revision": "eccd569f283535499f9b",
+    "url": "/static/js/main.55f6a273.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c5273f2486746fba6598b28c730c090.js"
+  "/precache-manifest.39960d2bd852c5ff9abed3e7fa084264.js"
 );
 
 self.addEventListener('message', (event) => {
